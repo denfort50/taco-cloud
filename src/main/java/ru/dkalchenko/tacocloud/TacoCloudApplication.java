3c6f84729e73005937f2335e19b54200eb844110
denfort50 <denfort50@yandex.ru>
2023-03-19 09:44:24 +0300

    Init commit

---
+++ src/main/java/ru/dkalchenko/tacocloud/TacoCloudApplication.java
@@ -0,0 +1,14 @@
+package ru.dkalchenko.tacocloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TacoCloudApplication.class, args);
+        System.out.println("http://localhost:8080");
+    }
+
+}
